7e96c3e5d474a0191c7dda6e9902abb464d7019d
Kelvin Graddick <kelvingraddick@kg.codes>
2021-03-02 07:59:48 -0500

    Plant screen: add plant and remove plant

--- Screens/PlantScreen.js
+++ Screens/PlantScreen.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar, ScrollView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 import CategoryButtonComponent from "../Components/CategoryButtonComponent";
 import Colors from '../Constants/Colors';
 import Fonts from '../Constants/Fonts';
+import { set } from "react-native-reanimated";
 
 const PlantScreen = ({ route, navigation }) => {
   const { id } = route.params;
   const [plant, setPlant] = useState();
+  const [add, setAdd] = useState();
 
   useEffect(() => {
     getPlant();
-    return () => getPlant();
+    getAdd();
   }, []);
 
   const getPlant = async function() {
     await firestore()
       .collection('plants')
       .doc(id)
-      .get()
-      .then(documentSnapshot => {
+      .onSnapshot(documentSnapshot => {
         if (documentSnapshot.exists) {
           var plant = documentSnapshot.data();
           setPlant(plant);
@@ -28,8 +30,50 @@ const PlantScreen = ({ route, navigation }) => {
       });
   };
 
-  const addPlant = async function() {
+  const getAdd = async function() {
+    await firestore()
+      .collection('adds')
+      .where('userId', '==', auth().currentUser.uid)
+      .where('plantId', '==', id)
+      .limit(1)
+      .onSnapshot(async collectionSnapshot => {
+        const adds = [];
+        collectionSnapshot.forEach(documentSnapshot => {
+          adds.push({ id: documentSnapshot.id, ...documentSnapshot.data() });
+        });
+        const add = adds[0];
+        if (add) {
+          setAdd(add);
+          console.info('Retrieved add from database: ' + JSON.stringify(add));
+        } else {
+          setAdd(undefined);
+          console.info('Did not find add in database for plant ' + id + ' and user ' + auth().currentUser.uid);
+        }
+      });
+  };
 
+  const addPlant = async function() {
+    firestore()
+      .collection('adds')
+      .add({
+        userId: auth().currentUser.uid,
+        plantId: id,
+      })
+      .then(() => {
+        //getAdd();
+        console.log('Plant ' + id + ' added for user ' + auth().currentUser.uid);
+      });
+  };
+
+  const removePlant = async function() {
+    firestore()
+      .collection('adds')
+      .doc(add.id)
+      .delete()
+      .then(() => {
+        //getAdd();
+        console.log('Add ' + add.id + ' removed for user ' + auth().currentUser.uid);
+      });
   };
 
   return (
@@ -39,12 +83,22 @@ const PlantScreen = ({ route, navigation }) => {
         <Text style={styles.titleText}>{plant?.commonName}</Text>
         <Text style={styles.subTitleText}>{plant?.scientificNames?.join(', ')}</Text>
         <Image style={styles.image} source={{ uri: plant?.imageUrl }} />
-        <TouchableOpacity
-          style={styles.addPlantButton}
-          onPress={() => addPlant()}
-          underlayColor='#fff'>
-          <Text style={styles.addPlantButtonText}>Add Plant</Text>
-        </TouchableOpacity>
+        { !add &&
+          <TouchableOpacity
+            style={styles.addPlantButton}
+            onPress={() => addPlant()}
+            underlayColor='#fff'>
+            <Text style={styles.addPlantButtonText}>Add Plant</Text>
+          </TouchableOpacity>
+        }
+        { add &&
+          <TouchableOpacity
+            style={styles.addPlantButton}
+            onPress={() => removePlant()}
+            underlayColor='#fff'>
+            <Text style={styles.addPlantButtonText}>Remove Plant</Text>
+          </TouchableOpacity>
+        }
         <Text style={styles.titleText}>Care</Text>
         <Text style={styles.subTitleText}>Days between watering · {plant?.daysBetweenWatering}</Text>
         <Text style={styles.subTitleText}>Days between resoiling · {plant?.daysBetweenResoiling}</Text>
